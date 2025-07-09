e80477608a14ee10004e3deecd3ab8b06178dbb8
DavEmmert <David.Emmert@online.de>
2025-07-09 19:55:04 +0200

    Text label + predictor out

---
+++ Class_TextLabelDataset.py
@@ -0,0 +1,13 @@
+class TextLabelDataset(Dataset):
+    def __init__(self, texts, labels):
+        self.texts = texts
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        return {
+            "text": self.texts[idx],
+            "label": self.labels[idx]
+        }
